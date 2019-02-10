4b9f2e327c3ab5fedb295af024c3d3e67753da76
pongGoo <yesjammaker@gmail.com>
2019-02-10 23:03:02 +0900

    스프라이트 오브젝트 만들기
    4x4로 스프라이트 오브젝트 만들기
    예시이미지 추가

---
+++ Main/N1/Assets/Scripts/ScrMain.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrMain : MonoBehaviour
+{
+
+    public int partitionColumnCount;
+    public GameObject[,] partitions = new GameObject[4,4];    
+    public Texture2D stageImage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        partitionColumnCount = 4;        
+        CreatePartition(partitionColumnCount);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreatePartition (int ColumnCount)
+    {
+        
+        for (int i=0; i < ColumnCount; i++)
+        {
+            for (int j = 0; j < ColumnCount; j++)
+            {
+                partitions[i, j] = new GameObject("partition" + i + "_" + j);
+                SpriteRenderer renderer = partitions[i, j].AddComponent<SpriteRenderer>();
+                renderer.sprite = Sprite.Create(stageImage, new Rect(0,0,stageImage.width, stageImage.height), new Vector2(0.5f, 0.5f), 100.0f);
+            }
+        }
+
+    }
+
+}
